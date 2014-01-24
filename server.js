3fb89c8d3eee17807f5144fe641cb34cf2a90118
Sampo Verkasalo <sampo.verkasalo@gmail.com>
2014-01-24 11:55:01 +0200

    Heroku debug

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ if (process.env.REDISTOGO_URL) {
   var rtg   = require("url").parse(process.env.REDISTOGO_URL);
   publisherClient = redis.createClient(rtg.port, rtg.hostname);
   publisherClient.auth(rtg.auth.split(":")[1]);
+  console.log('heroku redis');
 } else {
   publisherClient = redis.createClient();
 }
