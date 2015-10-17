64ad79a434db132453d6c6e5f954056a1e4c9686
Sampo Verkasalo <sampo.verkasalo@gmail.com>
2015-10-17 10:10:09 +0300

    Change player name to be unique on for not deleted players

--- models/player.js
+++ models/player.js
@@ -1,16 +1,8 @@
 module.exports = function(Sequelize, DataTypes) {
   var schema = {
-    id: {
-      primaryKey: true,
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4
-    },
-    name: { type: DataTypes.STRING, allowNull: false, unique: 'leaguePlayerName', validate: { len: [3,50] }},
-    LeagueId: {
-      type: DataTypes.UUID,
-      allowNull: false,
-      unique: 'leaguePlayerName'
-    },
+    id: { primaryKey: true, type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4 },
+    name: { type: DataTypes.STRING, allowNull: false, validate: { len: [3,50] }},
+    LeagueId: { type: DataTypes.UUID, allowNull: false },
     deleted: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false }
   };
   var Player = Sequelize.define("Player", schema, {
@@ -19,6 +11,9 @@ module.exports = function(Sequelize, DataTypes) {
           Player.belongsTo(models.League, {foreignKey: 'LeagueId', onDelete: "CASCADE" });
         }
       },
+      indexes: [
+        { unique: 'leaguePlayerName', fields: ['name', 'LeagueId'], where: { deleted: false } }
+      ],
       defaultScope: {
         where: {
           deleted: false
